8df94b06044a79a03c608164ef32650dce606238
wenaltszn <91277430+wenalt@users.noreply.github.com>
2025-09-07 15:31:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+// next.config.js
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    urlImports: ['https://cdn.jsdelivr.net']
-  }
+    urlImports: ['https://cdn.jsdelivr.net'],
+  },
+  webpack: (config) => {
+    // Redirige tous les imports 'react-spinners' vers le shim compatible ESM
+    config.resolve.alias['react-spinners'] = path.resolve(__dirname, 'lib/react-spinners-shim.js');
+    return config;
+  },
 };
+
 module.exports = nextConfig;
